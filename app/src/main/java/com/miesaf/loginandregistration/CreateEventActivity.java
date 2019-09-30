48984252d1180c627311e976154087e4318dfb1a
The Blue Moody <muhd.mirul95@gmail.com>
2019-09-30 09:54:14 +0800

    Initial commit

---
+++ app/src/main/java/com/miesaf/loginandregistration/CreateEventActivity.java
@@ -0,0 +1,50 @@
+package com.miesaf.loginandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CreateEventActivity extends AppCompatActivity {
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_MESSAGE = "message";
+    private static final String KEY_FULL_NAME = "full_name";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_EMPTY = "";
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etConfirmPassword;
+    private EditText etFullName;
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String fullName;
+    private ProgressDialog pDialog;
+    private String register_url = "https://miesaf.ml/dev/member/register.php";
+    private SessionHandler session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
